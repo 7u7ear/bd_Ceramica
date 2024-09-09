b75062a7799700ba6f4861d41d02e5e41f4ddc6e
Matias E Arnau <arnaumatias@gmail.com>
2024-09-08 21:27:40 -0300

    borrarestetica mejorada

--- agregar.php
+++ agregar.php
@@ -7,7 +7,9 @@
     <title>Agregar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="css/style.css">
+
+
 </head>
 
 <body>
@@ -55,50 +57,66 @@
     }
 
     ?>
-    <div class="container p-2">
-        < class="row m-2">
-        <div class="col bg-info p-3"></div>
-            <div class="col bg-info p-3">
+    <div class="container p-2 mt-5">
+    <div class="row"></div>
+    <div class="col-2 p-3"></div>    
+    <div class="row m-2">            
+            <div class="col-3  p-3"></div>
+            <div class="col-6 bg-primary p-3">
+                <h2 style="font-family: 'Courier New', Courier, monospace; color: white;">Formulario de Agentes:</h2>
                 <section class="contact-form">
-                    <h2>Formulario de Agentes</h2>
-                    <p>Por favor, complete los campos para agregar un nuevo agente.</p>
+
                     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 
                         <label for="id_rh">Id RH:</label>
                         <input type="text" id="id_rh" name="id_rh" required /><br><br>
 
-                        <label for="apellido_Nombre">Apellido y Nombre:</label>
+                        <label for="apellido_Nombre">Apellido/s y Nombre/s:</label> 
                         <input type="text" id="apellido_Nombre" name="apellido_Nombre" required /> <br><br>
 
                         <label for="cuil">CUIL:</label>
-                        <input type="text" id="cuil" name="cuil" required /><br><br>
+                        <input type="text" id="cuil" name="cuil" required />&nbsp;&nbsp;
 
                         <label for="dni">DNI:</label>
                         <input type="number" id="dni" name="dni" required /> <br><br>
 
                         <label for="ficha">FICHA:</label>
-                        <input type="number" id="ficha" name="ficha" required /> <br><br>
-
-                        <label for="IngresoEst">Fecha de Ingreso:</label>
-                        <input type="date" id="IngresoEst" name="IngresoEst" required /> <br><br>
-
-                        <label for="ultima_DJ">Última DJ:</label>
-                        <input type="date" id="ultima_DJ" name="ultima_DJ" required /><br><br>
+                        <input type="number" id="ficha" name="ficha" required /> &nbsp;&nbsp;&nbsp;&nbsp;
 
                         <label for="en_actividad">En Actividad:</label>
                         <input type="checkbox" id="en_actividad" name="en_actividad" value="1" /> <br><br>
 
-                        <input type="submit" class="btn btn-primary ms-6 mb-3 me-3" name="enviar" value="Agregar" />
-                        <a href="index.php" class="btn btn-primary ms-6 mb-3">Regresar</a>
+                        <label for="IngresoEst">Fecha de Ingreso:</label>
+                        <input type="date" id="IngresoEst" name="IngresoEst" required />&nbsp;&nbsp;
+
+                        <label for="ultima_DJ">Última DJ:</label>
+                        <input type="date" id="ultima_DJ" name="ultima_DJ" required /><br><br>                        
+
+                        <button type="submit" class="btn btn-primary ms-6 mb-3 me-3" name="enviar" style="font-family: 'Courier New', Courier, monospace; font-weight: normal;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                                <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5" />
+                            </svg>
+                            Agregar
+                        </button>
+
+                        <a href="index.php" class="btn btn-primary ms-6 mb-3" style="font-family: 'Courier New', Courier, monospace; font-weight: normal;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
+                                <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1" />
+                            </svg>
+                            Regresar
+                        </a>
 
                     </form>
                 </section>
 
             </div>
-            <div class="col bg-info p-3">
-        
+            <div class="col-3  p-3"> </div>
         </div>
-    </div>
 </body>
 
 </HTml>
+
+<footer class="footer pb-1" style="position: fixed; bottom: 0; width: 100%; font-family: 'Courier New', Courier, monospace; font-size: 20px; background-color: rgba(64, 142, 165, 0.5); text-align: center; padding: 20px; display: flex; align-items: center; justify-content: center; height: 40px;">
+    <p>© 2024 ESEA en Cerámica N°1. Escuela Superior de Enseñanza Artística de Cerámica. Bulnes 45, CABA.</p>
+</footer>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Ecn1</title>
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
@@ -26,7 +26,6 @@
     </nav>
 
 
-    <a href="agregar.php" class="btn btn-primary ms-6 " >Nuevo Agente</a>
 
     <?php
     include("coneccion.php");
@@ -34,64 +33,71 @@
     $result = mysqli_query($conn, $sql);
 
     ?>
-    <div id="fondo" class="flex-center">
-        <div class="container">
-
-            <!-- Formulario de contacto -->
-            <div class="container p-2">
-                <div class="row m-2">
-
-                    <div class="col-12 bg-info mt-5)">
-                        <h1> Listado Agentes </h1>
-                        <table>
-                            <thead>
+    <div class="container">
+        <a href="agregar.php" class="btn btn-primary ms-8 ">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5" />
+            </svg>&nbsp;&nbsp;Nuevo Agente
+        </a>
+        <!-- Formulario de contacto -->
+        <div class="container p-2">
+            <div class="row m-2">
+                <div class="col-12  mt-5)">
+                <h2 class="bg-primary p-2" style="font-family: 'Courier New', Courier, monospace; color: white;">Listado Agentes</h2>
+                    <table class="table table-striped table-bordered table-hover">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">Id_RH</th>
+                                <th scope="col">Apellido y Nombre</th>
+                                <th scope="col">Cuil</th>
+                                <th scope="col">Dni</th>
+                                <th scope="col">Ficha</th>
+                                <th scope="col">Fecha de Ingreso</th>
+                                <th scope="col">Última DJ</th>
+                                <th scope="col">En Actividad</th>
+                                <th scope="col">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            while ($row = mysqli_fetch_assoc($result)) {
+                            ?>
                                 <tr>
-                                    <th scope="col">Id_RH</th>
-                                    <th scope="col">Apellido y Nombre</th>
-                                    <th scope="col">Cuil</th>
-                                    <th scope="col">Dni</th>
-                                    <th scope="col">Ficha</th>
-                                    <th scope="col">Fecha de Ingreso</th>
-                                    <th scope="col">Última DJ</th>
-                                    <th scope="col">En Actividad</th>
-                                    <th scope="col">Acciones</th>
+                                    <td><?php echo $row['id_rh'] ?></td>
+                                    <td><?php echo $row['apellido_Nombre'] ?></td>
+                                    <td><?php echo $row['cuil'] ?></td>
+                                    <td><?php echo $row['dni'] ?></td>
+                                    <td><?php echo $row['ficha'] ?></td>
+                                    <td><?php echo $row['IngresoEst'] ?></td>
+                                    <td><?php echo $row['ultima_DJ'] ?></td>
+                                    <td><?php echo $row['en_actividad'] ?></td>
+                                    <td>
+                                        <?php echo "<a href =''>Editar</a>"; ?>
+                                        <?php echo "<a href=''>Eliminar</a>"; ?>
+                                    </td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                while ($row = mysqli_fetch_assoc($result)) {
-                                ?>
-                                    <tr>
-                                        <td><?php echo $row['id_rh'] ?></td>
-                                        <td><?php echo $row['apellido_Nombre'] ?></td>
-                                        <td><?php echo $row['cuil'] ?></td>
-                                        <td><?php echo $row['dni'] ?></td>
-                                        <td><?php echo $row['ficha'] ?></td>
-                                        <td><?php echo $row['IngresoEst'] ?></td>
-                                        <td><?php echo $row['ultima_DJ'] ?></td>
-                                        <td><?php echo $row['en_actividad'] ?></td>
-                                        <td>
-                                            <?php echo "<a href=''>Editar</a>"; ?>
-                                            <?php echo "<a href=''>Eliminar</a>"; ?>
-                                        </td>
-                                    </tr>
-                                <?php
-                                }
-                                ?>
-                            </tbody>
-                        </table>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
 
-
-                    </div>
 
                 </div>
-            </div>
 
-            <footer class="footer">
-                <p>Footer</p>
-                <?php
-                // Cerrar la conexión a la base de datos
-                $conn->close();
-                ?>
+            </div>
+        </div>
+        <?php
+        // Cerrar la conexión a la base de datos
+        $conn->close();
+        ?>
+    </div>
+</body>
 
 </html>
+
+<footer class="footer pb-1" style="position: fixed; bottom: 0; width: 100%; font-family: 'Courier New', Courier, monospace; font-size: 20px; background-color: rgba(64, 142, 165, 0.5); text-align: center; padding: 20px; display: flex; align-items: center; justify-content: center; height: 40px;">
+    <p> © 2024 ESEA en Cerámica N°1. Escuela Superior de Enseñanza Artística de Cerámica. Bulnes 45, CABA.</p>
+</footer>
+
